7855d48b13b8917b3d359948de2f6496708fb83b
Eduesparragoza1 <151690638+Eduesparragoza1@users.noreply.github.com>
2025-09-14 14:54:51 -0500

    eduesparragoza1.py
    Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/eduesparragoza1.py
@@ -0,0 +1,29 @@
+# https://www.python.org/
+# Comentario en una línea
+"""
+Esto es 
+un comentario
+en varias líneas
+"""
+
+'''
+Este también es otro comentaro
+en varias lineas
+'''
+
+Variable = "Una variable" #por convención
+
+Variable_constante = "No cambiar"
+Variable_constante ="hbhjvgvghsh"
+
+numero_cualquiera = 3
+nombre = "Edu, lo estas haciendo muy bien"
+Dec_num = 3.14
+Verd_Ment = true
+Verd_Ment = false
+
+
+
+print("!Hola, Python!")
+
+
